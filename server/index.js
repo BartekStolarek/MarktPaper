6c3584fe0cb062690e61f24a6aa4e16e40972fcf
BartekStolarek <bartek.stolarek@gmail.com>
2018-07-04 14:49:19 +0200

    Connect to mLab

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 const schema = require('./schema/schema');
 
 const app = express();
+
+//Connect to mlab database
+const mongoURI = '';
+
+mongoose.connect(mongoURI);
+mongoose.connection.once('open', () => {
+    console.log('Connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
